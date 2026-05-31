abca54a76c9df387961bddb144d5e57271d6d457
efolgar-2021545 <efolgar-2021545@kinal.edu.gt>
2026-05-30 18:39:14 -0600

    Se agrego la función y ruta de la imagen por Id

--- EcoKinal-Backend/AuthEcoKinal/helpers/generate-jwt.js
+++ EcoKinal-Backend/AuthEcoKinal/helpers/generate-jwt.js
@@ -5,7 +5,12 @@ export const generateJWT = (user) => {
   return jwt.sign(
     {
       uid: user.id,
-      role: user.roleId
+      role: user.roleId,
+      name: user.name || '',
+      username: user.username || '',
+      photo: user.image || null,
+      profilePicture: user.image || null,
+      image: user.image || null,
     },
     config.jwt.secret,
     {
