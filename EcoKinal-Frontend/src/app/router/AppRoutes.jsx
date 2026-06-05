9982e963dc6eaad197cdfad3751807fcb91cf3ea
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-06-05 08:58:49 -0600

    Se modifico la ruta de LandingPage

--- EcoKinal-Frontend/src/app/router/AppRoutes.jsx
+++ EcoKinal-Frontend/src/app/router/AppRoutes.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { PrivateRoute } from './PrivateRoute'
 
 // Auth pages
-import LandingPage        from '../../features/auth/pages/LandingPage'
+import {LandingPage}        from '../../features/auth/pages/LandingPage'
 import LoginPage          from '../../features/auth/pages/LoginPage'
 import RegisterPage       from '../../features/auth/pages/RegisterPage'
 import VerifyPage         from '../../features/auth/pages/VerifyPage'
